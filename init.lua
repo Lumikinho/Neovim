12b1f7cd304cd6a96ce1389aa8856410a8cb9fbf
Lumikinho <lucasmirandar11@gmail.com>
2025-07-19 23:11:05 -0300

    teste

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+-- Set shell to Git Bash
+vim.o.shell = "C:/Program Files/Git/bin/bash.exe"
+vim.o.shellcmdflag = "-c"
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.cmd("colorscheme gruvbox")
