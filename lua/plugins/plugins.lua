cefecad0a5c53a087c3ac91c796c9b06742e5c97
lumikinho <lucasmirandar11@gmail.com>
2025-07-21 22:04:04 -0300

    oi

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,5 +1,12 @@
 return {
   {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      direction = "float",
+    },
+  },
+  {
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
